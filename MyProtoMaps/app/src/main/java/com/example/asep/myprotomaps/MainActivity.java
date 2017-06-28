421a871cac8701cdf5e8ec1d3c0ff8e6f1917dd1
Arif Lukman Hakim <goodgamerasep@gmail.com>
2017-06-28 23:17:10 +0700

    map testing 1

--- MyProtoMaps/app/src/main/java/com/example/asep/myprotomaps/MainActivity.java
+++ MyProtoMaps/app/src/main/java/com/example/asep/myprotomaps/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.asep.myprotomaps;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,15 +11,26 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+public class MainActivity extends FragmentActivity implements AdapterView.OnItemSelectedListener, OnMapReadyCallback{
 
     Spinner spinner;
 
+    @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         spinner = (Spinner) findViewById(R.id.spnType);
 
@@ -34,5 +48,25 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Spinner spinner = (Spinner) findViewById(R.id.spnType);
+        String type = spinner.getSelectedItem().toString();
+        switch(type){
+            case "Normal":
+                googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                break;
+            case "Satellite":
+                googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                break;
+            case "Hybrid":
+                googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                break;
+            case "Terrain":
+                googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                break;
+        }
     }
 }
